290730df4a4839cb9fda36e1c91e46958027b73c
c13proto <c13proto@aqua.ocn.ne.jp>
2015-09-25 21:11:40 +0900

    マスクで合成するまで作成

---
+++ histgram/main.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace histgram
+{
+    partial class main
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_mask = new System.Windows.Forms.Button();
+            this.button_original = new System.Windows.Forms.Button();
+            this.textBox_x = new System.Windows.Forms.TextBox();
+            this.textBox_y = new System.Windows.Forms.TextBox();
+            this.pictureBoxIpl1 = new OpenCvSharp.UserInterface.PictureBoxIpl();
+            this.button_csv = new System.Windows.Forms.Button();
+            this.label_color = new System.Windows.Forms.Label();
+            this.textBox_mask = new System.Windows.Forms.TextBox();
+            this.button_合成 = new System.Windows.Forms.Button();
+            this.button_画像出力 = new System.Windows.Forms.Button();
+            this.trackBar_mask = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_mask)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_mask
+            // 
+            this.button_mask.Location = new System.Drawing.Point(2, 3);
+            this.button_mask.Name = "button_mask";
+            this.button_mask.Size = new System.Drawing.Size(75, 23);
+            this.button_mask.TabIndex = 0;
+            this.button_mask.Text = "マスク画像";
+            this.button_mask.UseVisualStyleBackColor = true;
+            this.button_mask.Click += new System.EventHandler(this.OnClick_マスク画像);
+            // 
+            // button_original
+            // 
+            this.button_original.Location = new System.Drawing.Point(2, 57);
+            this.button_original.Name = "button_original";
+            this.button_original.Size = new System.Drawing.Size(75, 23);
+            this.button_original.TabIndex = 1;
+            this.button_original.Text = "元画像";
+            this.button_original.UseVisualStyleBackColor = true;
+            this.button_original.Click += new System.EventHandler(this.OnClick_元画像);
+            // 
+            // textBox_x
+            // 
+            this.textBox_x.Location = new System.Drawing.Point(0, 86);
+            this.textBox_x.Name = "textBox_x";
+            this.textBox_x.Size = new System.Drawing.Size(29, 19);
+            this.textBox_x.TabIndex = 2;
+            this.textBox_x.Text = "x";
+            this.textBox_x.TextChanged += new System.EventHandler(this.TextChanged_x);
+            // 
+            // textBox_y
+            // 
+            this.textBox_y.Location = new System.Drawing.Point(30, 86);
+            this.textBox_y.Name = "textBox_y";
+            this.textBox_y.Size = new System.Drawing.Size(29, 19);
+            this.textBox_y.TabIndex = 3;
+            this.textBox_y.Text = "y";
+            this.textBox_y.TextChanged += new System.EventHandler(this.TextChanged_y);
+            // 
+            // pictureBoxIpl1
+            // 
+            this.pictureBoxIpl1.Location = new System.Drawing.Point(83, 3);
+            this.pictureBoxIpl1.Name = "pictureBoxIpl1";
+            this.pictureBoxIpl1.Size = new System.Drawing.Size(299, 273);
+            this.pictureBoxIpl1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxIpl1.TabIndex = 4;
+            this.pictureBoxIpl1.TabStop = false;
+            this.pictureBoxIpl1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MouseMove_PictureBoxIpl1);
+            // 
+            // button_csv
+            // 
+            this.button_csv.Location = new System.Drawing.Point(2, 253);
+            this.button_csv.Name = "button_csv";
+            this.button_csv.Size = new System.Drawing.Size(75, 23);
+            this.button_csv.TabIndex = 5;
+            this.button_csv.Text = "csv出力";
+            this.button_csv.UseVisualStyleBackColor = true;
+            // 
+            // label_color
+            // 
+            this.label_color.AutoSize = true;
+            this.label_color.Location = new System.Drawing.Point(65, 89);
+            this.label_color.Name = "label_color";
+            this.label_color.Size = new System.Drawing.Size(30, 12);
+            this.label_color.TabIndex = 6;
+            this.label_color.Text = "color";
+            // 
+            // textBox_mask
+            // 
+            this.textBox_mask.Location = new System.Drawing.Point(5, 32);
+            this.textBox_mask.Name = "textBox_mask";
+            this.textBox_mask.Size = new System.Drawing.Size(22, 19);
+            this.textBox_mask.TabIndex = 7;
+            this.textBox_mask.Text = "9";
+            this.textBox_mask.TextChanged += new System.EventHandler(this.TextChanged_mask);
+            // 
+            // button_合成
+            // 
+            this.button_合成.Location = new System.Drawing.Point(2, 205);
+            this.button_合成.Name = "button_合成";
+            this.button_合成.Size = new System.Drawing.Size(75, 23);
+            this.button_合成.TabIndex = 8;
+            this.button_合成.Text = "合成";
+            this.button_合成.UseVisualStyleBackColor = true;
+            this.button_合成.Click += new System.EventHandler(this.OnClick_合成);
+            // 
+            // button_画像出力
+            // 
+            this.button_画像出力.Location = new System.Drawing.Point(2, 229);
+            this.button_画像出力.Name = "button_画像出力";
+            this.button_画像出力.Size = new System.Drawing.Size(75, 23);
+            this.button_画像出力.TabIndex = 9;
+            this.button_画像出力.Text = "画像出力";
+            this.button_画像出力.UseVisualStyleBackColor = true;
+            // 
+            // trackBar_mask
+            // 
+            this.trackBar_mask.AutoSize = false;
+            this.trackBar_mask.Location = new System.Drawing.Point(30, 32);
+            this.trackBar_mask.Margin = new System.Windows.Forms.Padding(0);
+            this.trackBar_mask.Maximum = 20;
+            this.trackBar_mask.Minimum = 1;
+            this.trackBar_mask.Name = "trackBar_mask";
+            this.trackBar_mask.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.trackBar_mask.Size = new System.Drawing.Size(50, 19);
+            this.trackBar_mask.TabIndex = 10;
+            this.trackBar_mask.Value = 9;
+            this.trackBar_mask.ValueChanged += new System.EventHandler(this.ValueChanged_mask);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(389, 281);
+            this.Controls.Add(this.trackBar_mask);
+            this.Controls.Add(this.button_画像出力);
+            this.Controls.Add(this.button_合成);
+            this.Controls.Add(this.textBox_mask);
+            this.Controls.Add(this.label_color);
+            this.Controls.Add(this.button_csv);
+            this.Controls.Add(this.pictureBoxIpl1);
+            this.Controls.Add(this.textBox_y);
+            this.Controls.Add(this.textBox_x);
+            this.Controls.Add(this.button_original);
+            this.Controls.Add(this.button_mask);
+            this.Name = "main";
+            this.Text = "main";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_mask)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_mask;
+        private System.Windows.Forms.Button button_original;
+        private System.Windows.Forms.TextBox textBox_x;
+        private System.Windows.Forms.TextBox textBox_y;
+        private OpenCvSharp.UserInterface.PictureBoxIpl pictureBoxIpl1;
+        private System.Windows.Forms.Button button_csv;
+        private System.Windows.Forms.Label label_color;
+        private System.Windows.Forms.TextBox textBox_mask;
+        private System.Windows.Forms.Button button_合成;
+        private System.Windows.Forms.Button button_画像出力;
+        private System.Windows.Forms.TrackBar trackBar_mask;
+    }
+}
+
---
+++ histgram/main.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+namespace histgram
+{
+    public partial class main : Form
+    {
+        public static Mat マスク元画像;
+        public static Mat マスク画像;
+        public static Mat 元画像;
+        public static Mat 合成画像;
+
+        public main()
+        {
+            InitializeComponent();
+        }
+        void 膨張(ref Mat src)
+        {
+
+            if (マスク元画像 != null)
+            {
+                src = マスク元画像.Clone();
+                src = src.Threshold(254, 255, ThresholdTypes.Binary);
+
+                Mat element = Cv2.GetStructuringElement(MorphShapes.Rect, new OpenCvSharp.Size(trackBar_mask.Value, trackBar_mask.Value), new OpenCvSharp.Point(trackBar_mask.Value / 2, trackBar_mask.Value / 2));
+                src = src.MorphologyEx(MorphTypes.DILATE, element);
+                pictureBoxIpl1.ImageIpl = src;
+            }
+        }
+
+        void 合成(ref Mat src)
+        {
+            if (マスク画像 != null && 元画像 != null)
+            {
+                src = 元画像.Clone();
+                Cv2.Add(マスク画像, 元画像, src);
+                pictureBoxIpl1.ImageIpl = src;
+ 
+            }
+ 
+        }
+
+        private void OnClick_マスク画像(object sender, EventArgs e)
+        {
+            画像取得(ref マスク元画像);
+            膨張(ref マスク元画像);
+
+        }
+        private void OnClick_元画像(object sender, EventArgs e)
+        {
+            画像取得(ref 元画像);
+        }
+        void 画像取得(ref Mat src)
+        {
+            if (src != null)
+            {
+                src.Dispose();
+                src = new OpenCvSharp.Mat(480, 640, MatType.CV_8UC1);
+            }
+            OpenFileDialog dialog = new OpenFileDialog()
+            {
+                Multiselect = false,  // 複数選択の可否
+                Filter =  // フィルタ
+                "画像ファイル|*.bmp;*.gif;*.jpg;*.png|全てのファイル|*.*",
+            };
+            //ダイアログを表示
+            DialogResult result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+
+                //OKボタンがクリックされたとき
+                //選択されたファイル名をすべて表示する
+                foreach (var file in dialog.FileNames.Select((value, index) => new { value, index }))
+                {
+                    src = new Mat(file.value, ImreadModes.GrayScale);
+                }
+                // ファイル名をタイトルバーに設定
+                this.Text = dialog.FileNames[0];
+
+                pictureBoxIpl1.ImageIpl = src;
+            }
+        }
+
+
+
+        private void ValueChanged_mask(object sender, EventArgs e)
+        {
+            textBox_mask.Text = "" + trackBar_mask.Value;
+        }
+
+        private void TextChanged_mask(object sender, EventArgs e)
+        {
+            double isnumber;
+            if (double.TryParse(textBox_mask.Text, out isnumber))
+                if (isnumber >= trackBar_mask.Minimum && isnumber <= trackBar_mask.Maximum)
+                {
+                    trackBar_mask.Value = (int)isnumber;
+                    膨張(ref マスク画像);
+                }
+        }
+
+        private void MouseMove_PictureBoxIpl1(object sender, MouseEventArgs e)
+        {
+            System.Drawing.Point sp = System.Windows.Forms.Cursor.Position;
+            System.Drawing.Point cp = this.PointToClient(sp);
+            textBox_x.Text = "" + (cp.X - pictureBoxIpl1.Location.X);
+            textBox_y.Text = "" + (cp.Y - pictureBoxIpl1.Location.Y);
+        }
+        private void カーソル移動()
+        {
+            if (pictureBoxIpl1.ImageIpl != null)
+            {
+                double isnumber_x, isnumber_y;
+                if (double.TryParse(textBox_x.Text, out isnumber_x) && double.TryParse(textBox_y.Text, out isnumber_y))
+                    if ((isnumber_x >= 0 && isnumber_x <= pictureBoxIpl1.ImageIpl.Width) && (isnumber_y >= 0 && isnumber_y <= pictureBoxIpl1.ImageIpl.Height))
+                    {
+                        label_color.Text = pictureBoxIpl1.ImageIpl.At<Vec3b>((int)isnumber_y, (int)isnumber_x).Item0.ToString();
+                        //クライアント座標を画面座標に変換する
+                        System.Drawing.Point mp = this.PointToScreen(new System.Drawing.Point((int)isnumber_x + pictureBoxIpl1.Location.X, (int)isnumber_y + pictureBoxIpl1.Location.Y));
+                        //マウスポインタの位置を設定する
+                        System.Windows.Forms.Cursor.Position = mp;
+                    }
+            }
+        }
+
+        private void TextChanged_x(object sender, EventArgs e)
+        {
+            カーソル移動();
+        }
+
+        private void TextChanged_y(object sender, EventArgs e)
+        {
+            カーソル移動();
+        }
+
+        private void OnClick_合成(object sender, EventArgs e)
+        {
+            合成(ref 合成画像);
+        }
+
+    }
+}
